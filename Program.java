53ddd83533601b403716a8d609e9baf7d372adf4
ChosenChris <80711177+ChosenChris@users.noreply.github.com>
2021-04-14 13:55:54 +0200

    Improvements in the user interface.
    When executing the interpreter, a command line appears, with which the user can interact with the interpreter.

--- Program.java
+++ Program.java
@@ -17,18 +17,54 @@ public class Program {
 	 * gespeichert ist. Danach wird der Quellcode ausgefuehrt.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Interpreter for Lisp by Christian S\n");
-		System.out.println("Please enter a filename in which the source code is located.");
-		System.out.print("> ");
+		System.out.println("\n\nChosenChris' LispInterpreter [Version 4.0]\n");
 		
-		//Eingane des Dateinamen durch den Benutzer:
-		Scanner scannerObj = new Scanner(System.in);
-		String sFileName = scannerObj.next(); //Speichert den Dateinamen.
-		//Scanner nicht schliessen, da es sonst zu einem Fehler kommt: java.util.nosuchelementexception
-		//scannerObj.close();
-		
-		//Prozess starten, bei welchem der Lisp-Quellcode ausgefuehrt wird:
-		Controller controllerObj = new Controller(sFileName);
-		controllerObj.startController();
+		//Unendlichschleife laeuft solange, bis der user durch die Eingabe von "exit" den Interpreter beendet:
+		while (true) {
+			System.out.print("LispInterpreter>");
+			Scanner scannerObj = new Scanner(System.in);
+			String sInput = scannerObj.nextLine(); //Speichert die Eingabe des Benutzers.
+			String sLowerCaseInput = sInput.toLowerCase(); //Speichert die Eingabe ohne Grossbuchstaben.
+			//Scanner nicht schliessen, da es sonst zu einem Fehler kommt: java.util.nosuchelementexception
+			
+			//Input verarbeiten:
+			if (sLowerCaseInput.equals("exit")) {
+				//Programm soll beendet werden:
+				System.out.println("The execution of the program is terminated.");
+				return;
+			}
+			
+			else if (sLowerCaseInput.equals("help")) {
+				//Es sollen alle moeglichen Befehle ausgegeben werden:
+				System.out.println("HELP            Displays this overview of all possible inputs.");
+				System.out.println("EXIT            Terminates the execution of this application.");
+				System.out.println("CODE <file>     Loads and executes the Lisp source code from the respective file.");
+				System.out.println();
+			}
+			
+			else if (sLowerCaseInput.length() >= 4 && sLowerCaseInput.substring(0, 4).equals("code")) {
+				//Es soll Quellcode geladen werden:
+				if (sInput.length() == 4) {
+					//Es ist kein Dateiname vorhanden:
+					System.out.println("Error: No file name found.\n");
+					continue;
+				}
+				if (sInput.indexOf(' ') == -1) {
+					//Es existiert keine Trennung zwischen "code" und dem Dateinamen:
+					System.out.println("Error: Cannot differentiate between command and file name.\n");
+					continue;
+				}
+				String sFilename = sInput.substring(sInput.indexOf(' ') + 1, sInput.length());
+				//Quellcode ueber den Controller ausfueheren:
+				Controller controllerObj = new Controller(sFilename);
+				controllerObj.startController();
+				System.out.println("\n");
+			}
+			
+			else {
+				//Unbekannter Befehl:
+				System.out.println("Error: The command \"" + sInput + "\" is incorrect.\n");
+			}
+		}
 	}
 }
